a874bac591549e36d36ec8356f28391bb2b9371a
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-08-07 13:12:55 +0200

    created a div for the Authors of the pictures name

--- index.js
+++ index.js
@@ -21,6 +21,16 @@ fetch(
 )
   .then((res) => res.json())
   .then((data) => {
-    //console.log(data)
+    //console.log(data);
     document.body.style.backgroundImage = `url(${data.urls.full})`;
   });
+
+/**
+ * Challenge part 2: Display the image's author
+ *
+ * With the data you've already fetched, display the name of the
+ * image author on the page. They show up as the "user" in the data
+ * returned from the API.
+ *
+ * Don't worry about positioning the author in the lower-left yet.
+ */
