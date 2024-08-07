3e21e9618e371ffe589028cd1946b259a0e5596c
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-08-07 14:38:18 +0200

    Changed the  div into a p

--- index.js
+++ index.js
@@ -23,14 +23,14 @@ fetch(
   .then((data) => {
     //console.log(data);
     document.body.style.backgroundImage = `url(${data.urls.full})`;
+    /**
+     * Challenge part 2: Display the image's author
+     *
+     * With the data you've already fetched, display the name of the
+     * image author on the page. They show up as the "user" in the data
+     * returned from the API.
+     *
+     * Don't worry about positioning the author in the lower-left yet.
+     */
+    document.getElementById("authorsName").textContent = data.user.name;
   });
-
-/**
- * Challenge part 2: Display the image's author
- *
- * With the data you've already fetched, display the name of the
- * image author on the page. They show up as the "user" in the data
- * returned from the API.
- *
- * Don't worry about positioning the author in the lower-left yet.
- */
