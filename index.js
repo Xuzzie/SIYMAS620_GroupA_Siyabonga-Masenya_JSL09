0bc6c3baeb178f232508fb2a539f8b47effad548
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-08-07 16:39:08 +0200

    Added catch and  fetch  to the code to deal with errors

--- index.js
+++ index.js
@@ -35,4 +35,21 @@ fetch(
     document.getElementById(
       "authorsName"
     ).textContent = `By: ${data.user.name}`;
+  })
+  .catch((err) => {
+    // Use a default background image/author
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+)`;
+    document.getElementById("author").textContent = `By: Dodi Achmad`;
   });
+
+/**
+ * Challenge: Pull down the cryptocurrency data for dogecoin from the
+ * CoinGecko API and log it to the console
+ *
+ * Also add a .catch() method to console any errors that might occur to the console
+ */
+
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+  .then((res) => res.json())
+  .then((data) => console.error(err));
