665cff9a09640ea3b519ffe595af5fe7b1ddcb5b
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-08-08 11:17:32 +0200

    inserted js code to display the weather

--- index.js
+++ index.js
@@ -86,10 +86,12 @@ function getCurrentTime() {
 }
 
 setInterval(getCurrentTime, 1000);
+
 // This section confused me and troubled me a bit.
+
 navigator.geolocation.getCurrentPosition((position) => {
   fetch(
-    `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`
+    `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`
   )
     .then((res) => {
       if (!res.ok) {
@@ -98,6 +100,16 @@ navigator.geolocation.getCurrentPosition((position) => {
       return res.json();
     })
     .then((data) => {
-      console.log(data);
-    });
+      // console.log(data)
+      const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      document.getElementById("weather").innerHTML = `
+              <img src=${iconUrl} />
+          `;
+      document.getElementById("weather").innerHTML = `
+                <img src=${iconUrl} />
+                <p>${Math.round(data.main.temp)}º</p>
+                <p>${data.name}</p>
+            `;
+    })
+    .catch((err) => console.error(err));
 });
