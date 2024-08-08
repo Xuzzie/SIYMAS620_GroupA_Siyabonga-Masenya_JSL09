01e534ff025b4517b497babf533d0da5c0332441
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-08-08 10:16:31 +0200

    Added financial idicators

--- index.js
+++ index.js
@@ -55,6 +55,12 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
         <img src=${data.image.small} />
         <span>${data.name}</span>
     `;
+
+    document.getElementById("crypto").innerHTML += `
+    <p>🎯: $${data.market_data.current_price.usd}</p>
+    <p>👆: $${data.market_data.high_24h.usd}</p>
+    <p>👇: $${data.market_data.low_24h.usd}</p>
+`;
   })
 
   /**
