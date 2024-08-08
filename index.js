cddfd69cdbab337f7a42c08f861175ef2bb6fe80
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-08-08 11:24:00 +0200

    Added CSS formating for the weather  icon

--- index.js
+++ index.js
@@ -104,7 +104,8 @@ navigator.geolocation.getCurrentPosition((position) => {
       const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
       document.getElementById("weather").innerHTML = `
               <img src=${iconUrl} />
-          `;
+                    <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+                <p class="weather-city">${data.name}</p>`;
       document.getElementById("weather").innerHTML = `
                 <img src=${iconUrl} />
                 <p>${Math.round(data.main.temp)}º</p>
