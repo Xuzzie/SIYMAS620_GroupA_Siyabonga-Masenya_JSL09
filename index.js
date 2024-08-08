e3393fa06c043da7431a3ae31aa9591a886a5a12
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-08-08 10:40:04 +0200

    Added Time ID and added code  related to

--- index.js
+++ index.js
@@ -73,4 +73,16 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
   .catch((err) => console.error(err));
 
 const date = new Date();
-console.log(date.toLocaleTimeString("en-us", { timeStyle: "short" }));
+document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {
+  timeStyle: "short",
+});
+
+function getCurrentTime() {
+  const date = new Date();
+  document.getElementById("time").textContent = date.toLocaleTimeString(
+    "en-us",
+    { timeStyle: "short" }
+  );
+}
+
+setInterval(getCurrentTime, 1000);
